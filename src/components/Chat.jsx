95e43cf26d6492eba0651d0c0b96ff54eb45563c
devs8l <developer.stellar8@gmail.com>
2025-02-25 16:42:32 +0530

    new-height

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -5,7 +5,7 @@ const Chat = () => {
     return (
         <div className="drop-shadow-md bg-white  rounded-2xl flex flex-col overflow-hidden">
             {/* Make this wrapper full height and ensure child can scroll */}
-            <div className="flex-grow overflow-y-auto  ">
+            <div className="flex-grow overflow-y-auto h-[80vh] ">
                 <ChatInterface />
             </div>
         </div>
